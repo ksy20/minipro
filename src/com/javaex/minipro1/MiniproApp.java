12ce99df4fc87554465c0d47c5cc0ab1142185bc
강소이 <ktenny02@gmail.com>
2021-12-14 16:49:32 +0900

    14/12

---
+++ src/com/javaex/minipro1/MiniproApp.java
@@ -0,0 +1,16 @@
+package com.javaex.minipro1;
+
+public class MiniproApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("***************************************");
+		System.out.print("*"+"\t");
+		System.out.print("   "+"전화번호 관리 프로그램"+"           ");
+		System.out.println("*");
+		System.out.println("***************************************");
+
+		
+	}
+
+}
---
+++ src/com/javaex/minipro1/Person.java
@@ -0,0 +1,11 @@
+package com.javaex.minipro1;
+
+public class Person {
+	
+	private String name;
+	private String hp;
+	private String company;
+	
+	
+
+}
